dc41d6792884c767362823e4290a0c2712242231
Atindra Mohan das <atindramohandas353@gmail.com>
2023-09-12 12:41:21 +0530

    fix cord

--- LoanCalculation.js
+++ LoanCalculation.js
@@ -3,17 +3,23 @@ import bodyParser from 'body-parser';
 
 const app = express();
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 
 
 app.post('/api/calculateLoan', (req, res) => {
     try {
         const { loanAmount, loanTermYears, loanTermMonths, interestRate, repaymentFrequency } = req.body;
         
-        console.log('Loan Amount:', loanAmount);
-        console.log('Loan Term (Years):', loanTermYears);
-        console.log('Loan Term (Months):', loanTermMonths);
-        console.log('Interest Rate (%):', interestRate);
-        console.log('Repayment Frequency:', repaymentFrequency);
+        // console.log('Loan Amount:', loanAmount);
+        // console.log('Loan Term (Years):', loanTermYears);
+        // console.log('Loan Term (Months):', loanTermMonths);
+        // console.log('Interest Rate (%):', interestRate);
+        // console.log('Repayment Frequency:', repaymentFrequency);
 
         
         if (!loanAmount || !loanTermYears || !interestRate || !repaymentFrequency) {
@@ -57,6 +63,8 @@ app.post('/api/calculateLoan', (req, res) => {
             totalRepayment: totalRepayment.toFixed(2),
             totalInterest: totalInterest.toFixed(2),
         };
+
+        console.log("Calculation Result:", result);
 
         
         res.json(result);
